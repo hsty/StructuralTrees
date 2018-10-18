ee5eb54ba9059bdc68d9ac3e1cea3167450f1405
hshett002c <Hitesh_Shetty2@cable.comcast.com>
2018-10-18 00:55:57 -0400

    Making it legible

--- src/main/scala/TryOp.scala
+++ src/main/scala/TryOp.scala
@@ -38,18 +38,18 @@ object TryOp extends App {
     def union(other: IntSet) = other
   }
 
-  case class NonEmpty(x: Int, l: IntSet, r: IntSet) extends IntSet {
-    def contains(t: Int): Boolean =
-      if(t < x) l contains t
-      else if (t > x) r contains t
+  case class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet {
+    def contains(x: Int): Boolean =
+      if(x < elem) left contains x
+      else if (x > elem) right contains x
       else true
 
-    def incl(t: Int): IntSet =
-      if(t < x) NonEmpty(x, l incl t, r)
-      else if (t > x) NonEmpty(x, l, r incl t)
+    def incl(x: Int): IntSet =
+      if(x < elem) NonEmpty(elem, left incl x, right)
+      else if (x > elem) NonEmpty(elem, left, right incl x)
       else this
 
-    def union(other: IntSet): IntSet = (l union (r union (other))) incl x
+    def union(other: IntSet): IntSet = (left union (right union (other))) incl elem
   }
 
 
